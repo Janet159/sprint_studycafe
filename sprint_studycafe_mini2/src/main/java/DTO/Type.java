41c757d5f91e69e832ef61746b6bf527beb53342
jaderuby2109 <jaderuby2109@gmail.com>
2025-05-21 12:47:02 +0900

    DTO작성

---
+++ sprint_studycafe_mini2/src/main/java/DTO/Type.java
@@ -0,0 +1,31 @@
+package board.DTO;
+
+import com.alohaclass.jdbc.annotation.Pk;
+import com.alohaclass.jdbc.annotation.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Table("Type")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Type {
+	@Pk
+	/** 
+	 * 구분자
+	 * 1:1 문의사항 : BOARD
+	 * 공지사항 : NOTICE
+	 * 편의시설 : SERVICE
+	 */
+	private String type;
+
+	@Pk
+	/** 구분 */
+	private int typeNo;
+	/** 구분명 */
+	private String typeName;
+}
